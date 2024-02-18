f79346db208dd6578c99b34ad3e72d00ea1618c5
readul-islam <developer.readul@gmail.com>
2024-02-18 10:07:07 +0600

    add all images and icons

---
+++ frontend/src/assets/icons/reactIcons/index.js
@@ -0,0 +1,41 @@
+import { MdOutlineGridView } from "react-icons/md";
+import { LuFilter } from "react-icons/lu";
+import { LuFileText } from "react-icons/lu";
+import { BiSolidBookHeart } from "react-icons/bi";
+import { MdOutlineSettings } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
+import { CiSearch } from "react-icons/ci";
+import { FiMenu } from "react-icons/fi";
+import { MdOutlineStar } from "react-icons/md";
+import { MdOutlineStarBorder } from "react-icons/md";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { TiPlus } from "react-icons/ti";
+import { MdLogin } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+
+
+export {
+    MdOutlineGridView as GridIcon,
+    LuFilter as FilterIcon,
+    LuFileText as RecipeIcon,
+    BiSolidBookHeart as FavoriteIcon,
+    MdOutlineSettings as SettingIcon,
+    IoNotifications as NotificationIcon,
+    CiSearch as SearchIcon,
+    FiMenu as MenuIcon,
+    MdOutlineStar as StarFillIcon,
+    MdOutlineStarBorder as StartEmptyIcon,
+    MdOutlineWatchLater as TimeIcon,
+    IoMdArrowRoundForward as ForwardIcon,
+    IoMdArrowRoundBack as BackwardIcon,
+    TiPlus as PlusIcon,
+    MdLogin as LoginIcon,
+    MdOutlineKeyboardArrowLeft as KeyboardArrowLeftIcon,
+    MdKeyboardArrowRight as KeyboardArrowRightIcon,
+ 
+
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary:{
+        light:"#FB9300",
+        default:"#F68712"
+      },
+      secondary:{
+        default:"#FEF5EC",
+      },
+      accent:{
+        default:"#F9F9F9",
+        light:"#FAF9FB"
+      },
+      sideBarText:{
+        default:"#2C2151",
+        light:"#B9B2D1"
+      },
+      heighLight:{
+        focus:"#FF7733"
+      }
+    },
     extend: {},
   },
   plugins: [],
